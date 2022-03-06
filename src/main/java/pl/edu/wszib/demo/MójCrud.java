5f1ea0d2f44a8e3b6b23a23d24d218cf457762e1
muzychka <TarasMuzychka1989@gmail.com>
2022-03-06 16:33:50 +0100

    06.03.2022(new)

---
+++ src/main/java/pl/edu/wszib/demo/MójCrud.java
@@ -0,0 +1,86 @@
+package pl.edu.wszib.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/ToDo")
+@RestController
+public class MójCrud {
+    public enum Status {
+        NEW,
+        IN_PROGRESS,
+        DONE
+    }
+    public static class ToDo {
+        public Integer id;
+        public String nazwa;
+        public Status status;
+
+        public Status getStatus() {
+            return status;
+        }
+
+        public void setStatus(Status status) {
+            this.status = status;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+        public void setId(Integer id) {
+            this.id = id;
+        }
+        public String getNazwa() {
+            return nazwa;
+        }
+        public void setNazwa(String nazwa) {
+            this.nazwa = nazwa;
+        }
+    }
+       public Integer idCounter = 1;
+        public Map<Integer, ToDo> kontener = new HashMap<>();
+
+    @PostMapping
+    public MójCrud.ToDo create(@RequestBody MójCrud.ToDo toDo){
+        toDo.id = idCounter++;
+        kontener.put(toDo.id, toDo);
+        return toDo;
+    }
+    @GetMapping
+    public List<ToDo> list(@RequestParam(required = false) MójCrud.Status status ){
+        if (status == null){
+            return new ArrayList<>(kontener.values());
+        }
+        List<ToDo> pasują = new ArrayList<>();
+        for (ToDo toDo : kontener.values()){
+            if (toDo.getStatus() == status){
+                pasują.add(toDo);
+            }
+        }
+        return pasują;
+    }
+    @GetMapping("/{id}")
+    public MójCrud.ToDo  get(@PathVariable Integer id){
+        return kontener.get(id);
+    }
+    @PutMapping
+    public MójCrud.ToDo  update(@RequestBody MójCrud.ToDo  toDo){
+        if (kontener.containsKey(toDo.id)){
+            kontener.put(toDo.id, toDo);
+            return toDo;
+        }
+        return null;
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        kontener.remove(id);
+    }
+}
+
+
+
+
